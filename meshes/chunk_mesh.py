3f691bffb411e8c1af990f39f33e588a1cab6dcb
pratim994 <aryanpratim86@gmail.com>
2025-12-28 13:13:38 +0530

    added terrain generation

--- meshes/chunk_mesh.py
+++ meshes/chunk_mesh.py
@@ -27,3 +27,4 @@ class ChunkMesh(BaseMesh):
 
         )
         return mesh
+        
---
+++ textures.py
@@ -0,0 +1,40 @@
+import pygame as pg
+import moderngl as mgl
+
+class Textures:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+
+        self.texture_0 = self.load('frame.png')
+        self.texture_1 = self.load('water.png')
+        self.texture_array_0 = self.load('tex_array_0.png', is_tex_array=True)
+
+        self.texture_0.use(location=0)
+        self.texture_array_0.use(location=1)
+        self.texture_1.use(location=2)
+
+    def load(self, file_name, is_tex_array=False):
+        texture = pg.image.load(f'assests/{file_name}')
+        texture = pg.transform.flip(texture, file_x=True, flip_y =False)
+
+        if is_tex_array:
+            num_layers = 3*texture.get_height()//texture.get_width()
+            texture = self.app.ctx.texture_array(
+                size = (texture.get_width(),texture.get_height()//num_layers, num_layers),
+                components = 4,
+                data = pg.image.tostring(texture, 'RGBA')
+            )
+
+        else:
+            texture = self.ctx.texture(
+                size=texture.get_width(),
+                components = 4,
+                data = pg.image.tostring(texture, 'RGB',False)
+            )
+            texture.anisotropy = 32.0
+            texture.build_mipmaps()
+            texture.filter = (mgl.NEAREST, mgl.NEAREST)
+
+
+            return texture
