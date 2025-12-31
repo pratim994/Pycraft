0a305598772597c0fb7d3e227b3255146f97dd0b
pratim994 <aryanpratim86@gmail.com>
2025-12-31 18:25:04 +0530

    vm

---
+++ world_objects/cloud.py
@@ -0,0 +1,13 @@
+from settings import *
+from meshes.cloud_mesh import CloudMesh
+
+class Cloud:
+    def __init__(self, app):
+        self.app = app
+        self.mesh = CloudMesh(app)
+
+    def update(self):
+        self.mesh.program['u_time'] = self.app.time
+
+    def render(self):
+        self.mesh.render()
